dd6ca09fdca4524977b08c2350c7814c1830dd57
rexfort9 <92596074+rexfort9@users.noreply.github.com>
2022-10-09 16:10:54 +0300

    Update dbo.GOODS.sql

--- dbo.GOODS.sql
+++ dbo.GOODS.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[GOODS] (
     [goods_id]             INT          IDENTITY (1, 1) NOT NULL,
-	[shop_id]              INT			NOT NULL, 
+    [shop_id]              INT			NOT NULL, 
     [goods_name]           VARCHAR (50) NOT NULL,
     [goods_price_onepiece] INT          NOT NULL,
     [goods_quentity]       INT          NOT NULL,
